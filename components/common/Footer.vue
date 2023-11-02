f1342ee7eee2b7f5ed649fdca5166debe98f522b
Daphne de Waal <d@waal.me>
2023-11-03 00:32:39 +0100

    Update license link in Footer.vue

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -6,7 +6,7 @@
           <q title="random nonsense">{{ quote }}</q>
         </p>
         <p>tenpi is created by daph</p>
-        <small>code licensed under <a href="/LICENSE">GPLv3</a></small>
+        <small>code licensed under <a href="/LICENSE.txt">GPLv3</a></small>
       </ELCenter>
     </footer>
   </ELBox>
