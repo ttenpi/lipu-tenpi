5917d43c6840d4e93eda7da583ffdf4989889093
Daphne de Waal <d@waal.me>
2023-11-04 16:42:04 +0100

    Remove font files and update font imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@
 import { defineNuxtConfig } from "nuxt/config"
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://api.fontshare.com/v2/css?f[]=general-sans@701,1,2,401,400,700&f[]=poppins@2,900,901,701,401,400,1,700&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   css: ["@/assets/style/style.css"],
   components: {
@@ -15,8 +29,8 @@ export default defineNuxtConfig({
     },
   },
   colorMode: {
-    preference: 'system',
-    fallback: 'latte',
-    classSuffix: '-theme'
-  }
+    preference: "system",
+    fallback: "latte",
+    classSuffix: "-theme",
+  },
 })
