ab83707ff17bbe48b4b5c18783eb9979d3d16600
Daphne de Waal <d@waal.me>
2023-12-10 01:14:11 +0100

    Update SpeedInsights import in app.vue

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-  import { SpeedInsights } from "@vercel/speed-insights/vue"
+  import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
   useHead({
     titleTemplate: (titleChunk) => {
