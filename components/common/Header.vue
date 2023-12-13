ba086253a929734b123d8393b5a1e17eeb2b0cfb
Daphne de Waal <d@waal.me>
2023-12-14 00:11:46 +0100

    Refactor header component

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,46 +1,44 @@
 <template>
-  <header>
-    <ELBox>
-      <ELSwitcher :class="$style.headerContainer" space="var(--s-1)">
-        <div class="font-display">
-          <NuxtLink to="/" style="color: var(--color-primary)">tenpi</NuxtLink>
-        </div>
+  <ELBox tag="header">
+    <ELSwitcher :class="$style.headerContainer" space="var(--s-1)">
+      <div class="font-display">
+        <NuxtLink to="/" style="color: var(--color-primary)">tenpi</NuxtLink>
+      </div>
 
-        <nav :class="$style.headerCenter">
-          <ul>
-            <li><NuxtLink to="/about">about</NuxtLink></li>
-            <!-- <li><NuxtLink to="/blog">blog</NuxtLink></li>
+      <nav :class="$style.headerCenter">
+        <ul>
+          <li><NuxtLink to="/about">about</NuxtLink></li>
+          <!-- <li><NuxtLink to="/blog">blog</NuxtLink></li>
             <li><NuxtLink to="/projects">projects</NuxtLink></li> -->
-            <li>
-              <NuxtLink to="https://tech.lgbt/@daph" rel="me" target="_blank">
-                <Icon name="mdi:mastodon" title="Mastodon on tech.lgbt" />
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="https://toki.social/@daph" rel="me" target="_blank">
-                <Icon name="IconsTokiPona" title="Mastodon on toki.social" />
-              </NuxtLink>
-            </li>
-          </ul>
-        </nav>
+          <li>
+            <NuxtLink to="https://tech.lgbt/@daph" rel="me" target="_blank">
+              <Icon name="mdi:mastodon" title="Mastodon on tech.lgbt" />
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="https://toki.social/@daph" rel="me" target="_blank">
+              <Icon name="IconsTokiPona" title="Mastodon on toki.social" />
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
 
-        <div :class="$style.headerEnd">
-          <div>
-            <label for="colorModePreference">theme:</label>
-            <select v-model="$colorMode.preference" id="colorModePreference">
-              <option value="system">System</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-              <option value="latte">🌻 Latte</option>
-              <option value="frappe">🪴 Frappe</option>
-              <option value="macchiato">🌺 Macchiato</option>
-              <option value="mocha">🌿 Mocha</option>
-            </select>
-          </div>
+      <div :class="$style.headerEnd">
+        <div>
+          <label for="colorModePreference">theme:</label>
+          <select v-model="$colorMode.preference" id="colorModePreference">
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="latte">🌻 Latte</option>
+            <option value="frappe">🪴 Frappe</option>
+            <option value="macchiato">🌺 Macchiato</option>
+            <option value="mocha">🌿 Mocha</option>
+          </select>
         </div>
-      </ELSwitcher>
-    </ELBox>
-  </header>
+      </div>
+    </ELSwitcher>
+  </ELBox>
 </template>
 
 <style module>
