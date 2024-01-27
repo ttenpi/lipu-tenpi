78368b64f1392b7e2097c309f9bdf885c66748b0
Daphne de Waal <d@waal.me>
2024-01-27 20:24:52 +0100

    toki pona typography improvements

---
+++ components/TP.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="tag" :lang="[sp ? 'tok-x-stlnpona' : 'tok-Latn']">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+  withDefaults(
+    defineProps<{
+      tag?: string
+      sp?: boolean
+    }>(),
+    {
+      tag: "span",
+      script: false,
+    }
+  )
+</script>
